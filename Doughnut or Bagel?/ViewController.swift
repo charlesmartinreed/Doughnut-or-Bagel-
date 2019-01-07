2728159856262bf2dacf850d6d65b04875828606
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-07 14:10:45 -0600

    Implement image picker (using photo library).

--- Doughnut or Bagel?/ViewController.swift
+++ Doughnut or Bagel?/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var doughnutConfidenceLabel: UILabel!
+    @IBOutlet weak var bageConfidenceLabel: UILabel!
+    
+    //MARK:- Properties
+    var picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+        
     }
 
+    //MARK:- IBActions
+    @IBAction func choosePhotoTapped(_ sender: UIBarButtonItem) {
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func processImage(image: UIImage) {
+        print("processing image!")
+    }
+}
 
+extension ViewController : UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //we need to turn the chosen item into a photo
+        if let image = info[.originalImage] as? UIImage {
+            //display the image and then process it so we can update the prediction labels
+            imageView.image = image
+            processImage(image: image)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController : UINavigationControllerDelegate {
+    
 }
 
